90f13f36215e1d542c7b9422a7c98ee32342bf61
Eben <me@ebenroux.co.za>
2015-11-22 07:46:03 +0200

    - v4.0.7

--- Shuttle.Core.Infrastructure/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Infrastructure/Properties/AssemblyInfo.cs
@@ -14,10 +14,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.Infrastructure for .NET Framework 4.5.1")]
 #endif
 
-[assembly: AssemblyVersion("4.0.6.0")]
+[assembly: AssemblyVersion("4.0.7.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("4.0.6")]
+[assembly: AssemblyInformationalVersion("4.0.7")]
 [assembly: ComVisible(false)]
