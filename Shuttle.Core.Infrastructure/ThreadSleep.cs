50c512ca6da57c0fbf8d79608e6227cb99cdd89b
jowenece <jowenece@gmail.com>
2016-11-07 12:19:55 -0500

    - update ThreadSleep accuracy

--- Shuttle.Core.Infrastructure/ThreadSleep.cs
+++ Shuttle.Core.Infrastructure/ThreadSleep.cs
@@ -1,23 +1,42 @@
+using System;
 using System.Threading;
 
 namespace Shuttle.Core.Infrastructure
 {
     public static class ThreadSleep
     {
+        public const int MaxStepSize = 1000;
+
         public static void While(int ms, IThreadState state)
         {
-            While(ms, state, 5);
+            // step size should be as large as possible,
+            // max step size by default
+            While(ms, state, MaxStepSize);
         }
 
         public static void While(int ms, IThreadState state, int step)
         {
-            var elapsed = 0;
+            // don't sleep less than zero
+            if (ms < 0)
+                return;
 
-            while (state.Active && elapsed < ms)
+            // step size must be positive and less than max step size
+            if (step < 0 || step > MaxStepSize)
+                step = MaxStepSize;
+
+            // end time
+            var end = DateTime.Now.AddMilliseconds(ms);
+            
+            // sleep time should be 
+            // 1) as large as possible to reduce burden on the os and improve accuracy
+            // 2) less than the max step size to keep the thread responsive to thread state
+
+            var remaining = (int)(end - DateTime.Now).TotalMilliseconds;
+            while (state.Active && remaining > 0)
             {
-                Thread.Sleep(step);
-
-                elapsed += step;
+                var sleep = remaining < step ? remaining : step;
+                Thread.Sleep(sleep);
+                remaining = (int)(end - DateTime.Now).TotalMilliseconds;
             }
         }
     }
