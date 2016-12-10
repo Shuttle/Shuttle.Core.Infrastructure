8d54fed9e0ea0d2fd99823d21460e753582f5608
Eben <me@ebenroux.co.za>
2016-12-10 06:50:03 +0200

    - ThreadSleep fixes

--- Shuttle.Core.Infrastructure/ThreadSleep.cs
+++ Shuttle.Core.Infrastructure/ThreadSleep.cs
@@ -16,22 +16,24 @@ namespace Shuttle.Core.Infrastructure
 
         public static void While(int ms, IThreadState state, int step)
         {
-            // don't sleep less than zero
             if (ms < 0)
+            {
                 return;
+            }
 
-            // step size must be positive and less than max step size
             if (step < 0 || step > MaxStepSize)
+            {
                 step = MaxStepSize;
+            }
 
-            // end time
             var end = DateTime.UtcNow.AddMilliseconds(ms);
             
-            // sleep time should be 
-            // 1) as large as possible to reduce burden on the os and improve accuracy
-            // 2) less than the max step size to keep the thread responsive to thread state
+            // sleep time should be:
+            // - as large as possible to reduce burden on the os and improve accuracy
+            // - less than the max step size to keep the thread responsive to thread state
 
             var remaining = (int)(end - DateTime.UtcNow).TotalMilliseconds;
+
             while (state.Active && remaining > 0)
             {
                 var sleep = remaining < step ? remaining : step;
