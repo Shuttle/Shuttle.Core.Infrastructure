be11f3109a81a006f31916e4ec0b45b238c53188
Eben <me@ebenroux.co.za>
2016-12-12 11:25:07 +0200

    - minor fixes

--- Shuttle.Core.Infrastructure/ComponentContainer/ComponentResolverExtensions.cs
+++ Shuttle.Core.Infrastructure/ComponentContainer/ComponentResolverExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Shuttle.Core.Infrastructure
 {
@@ -12,7 +15,7 @@ namespace Shuttle.Core.Infrastructure
         /// <returns>An instance of the type implementing the requested service type.</returns>
         public static T Resolve<T>(this IComponentResolver resolver) where T : class
         {
-            Guard.AgainstNull(resolver, "registry");
+            Guard.AgainstNull(resolver, "resolver");
 
             return (T)resolver.Resolve(typeof(T));
         }
@@ -36,7 +39,7 @@ namespace Shuttle.Core.Infrastructure
         /// <returns>An instance of the type implementing the requested service type if it can be resolved; else null.</returns>
         public static object AttemptResolve(this IComponentResolver resolver, Type serviceType)
         {
-            Guard.AgainstNull(resolver, "registry");
+            Guard.AgainstNull(resolver, "resolver");
             Guard.AgainstNull(serviceType, "serviceType");
 
             try
@@ -47,6 +50,28 @@ namespace Shuttle.Core.Infrastructure
             {
                 return null;
             }
+        }
+
+        /// <summary>
+        /// Instantiates (resolves) all the given types.  These may be types that will not necessarily be injected into another class but that may require other instances from the resolver.
+        /// </summary>
+        /// <param name="resolver">The resolver instance that contains the registered services.</param>
+        /// <param name="serviceTypes">The list of service types that need to be resolved.</param>
+        public static IEnumerable<object> Instantiate(this IComponentResolver resolver, IEnumerable<Type> serviceTypes)
+        {
+            Guard.AgainstNull(resolver, "resolver");
+
+            var result = new List<object>();
+            var types = serviceTypes as IList<Type> ?? serviceTypes.ToList();
+
+            if (serviceTypes == null || !types.Any())
+            {
+                return result;
+            }
+
+            result.AddRange(types.Select(resolver.Resolve));
+
+            return result;
         }
     }
 }
